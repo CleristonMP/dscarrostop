a4e9f1366c7b09578faa736783a24c7be2e4453f
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-12 22:24:17 -0300

    Section Cars

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header";
+import Catalog from "./routes/Catalog";
 
 export default function App() {
   return (
-    <Header />
+    <Catalog />
   );
 }
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import SectionCars from "./SectionCars";
+import "./styles.css";
+
+export default function Catalog() {
+  return (
+    <>
+      <Header />
+      <main>
+        <SectionCars />
+      </main>
+    </>
+  );
+}
