8a995455bf864236f0bb818c10177deef3bc5d1c
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-12 23:08:39 -0300

    Footer

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import SectionCars from "./SectionCars";
 import SectionComments from "./SectionComments";
@@ -11,6 +12,7 @@ export default function Catalog() {
         <SectionCars />
         <SectionComments />
       </main>
+      <Footer />
     </>
   );
 }
